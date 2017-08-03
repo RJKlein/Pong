9f4feffae646be3c392e766ea5de89535e1f147b
Rob Klein <rj.klein@comcast.net>
2017-08-03 17:39:18 -0500

    mainState

--- js/game.js
+++ js/game.js
@@ -74,7 +74,7 @@ Game.addNewPlayer = function(id,master,y){
     Game.master = master;
     Game.id = id;
     if (Game.master != Game.id){
-        main.startGame();
+        mainState.startGame();
     }; 
 };
 
