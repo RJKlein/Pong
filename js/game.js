ca6a847ec2209547d2809066050cff608f40323b
Rob Klein <rj.klein@comcast.net>
2017-08-05 11:18:53 -0500

    corrected scope of ball

--- js/game.js
+++ js/game.js
@@ -178,7 +178,7 @@ var playState = {
             }
         
             game.physics.arcade.velocityFromAngle(randomAngle, gameProperties.ballVelocity, this.ballSprite.body.velocity);
-            Client.sendNewBall(x, y, randomAngle, gameProperties.ballVelocity);
+            Client.sendNewBall(this.ballSprite.x, this.ballSprite.x, randomAngle, gameProperties.ballVelocity);
         }
     },
     
