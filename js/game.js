84648fb6fd869a79daba0a57d4541184423ab3a8
Rob Klein <rj.klein@comcast.net>
2017-08-07 12:42:57 -0500

    negated bounce

--- js/game.js
+++ js/game.js
@@ -86,7 +86,7 @@ var playState = {
             
             if (this.ballSprite.body.blocked.up || this.ballSprite.body.blocked.down || this.ballSprite.body.blocked.left || this.ballSprite.body.blocked.right) {
                 this.sndBallBounce.play();
-                Client.sendNewBall(this.ballSprite.x, this.ballSprite.y, Math.round(this.ballSprite.body.angle * (180/Math.PI)), this.ballVelocity);
+                Client.sendNewBall(this.ballSprite.x, this.ballSprite.y, (-1*Math.round(this.ballSprite.body.angle * (180/Math.PI))), this.ballVelocity);
             }
         }
     },
